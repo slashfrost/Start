341feb35994bba66556b3474f7de92139dac7a2f
sasha <morozalexander82@gmail.com>
2019-09-04 09:42:24 +0300

    deleted some unused strings and reformated code

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,17 +7,16 @@ public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         String[] numbers_str;
-        Integer [] numbers;
-        Integer count=0;
-        Integer sum=0;
+        Integer count = 0;
+        Integer sum = 0;
         System.out.println("Input text:");
         String str = in.nextLine();
         String str1 = str.replaceAll("\\D+", "");
         System.out.println(str1);
         numbers_str = str1.split("");
-        for (int i = 0; i < numbers_str.length ; i++) {
-            count=Integer.parseInt(numbers_str[i]);
-            sum=sum+count;
+        for (int i = 0; i < numbers_str.length; i++) {
+            count = Integer.parseInt(numbers_str[i]);
+            sum = sum + count;
         }
         System.out.println(sum);
     }

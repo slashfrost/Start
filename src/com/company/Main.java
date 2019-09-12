c252a4aca4e2561012e90c7f3636258055cb4b8f
sasha <morozalexander82@gmail.com>
2019-09-12 11:40:57 +0300

    added Stream API

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,16 +1,23 @@
 package com.company;
 
-import java.util.Scanner;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Main {
 
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        String file="/home/sasha/IdeaProjects/Start/src/com/company/Input";
+        List<String>lines=readLineByLine();
         String[] numbers_str;
         Integer count = 0;
         Integer sum = 0;
-        System.out.println("Input text:");
-        String str = in.nextLine();
+        String str=lines.toString();
         String str1 = str.replaceAll("\\D+", "");
         System.out.println(str1);
         numbers_str = str1.split("");
@@ -18,7 +25,19 @@ public class Main {
             count = Integer.parseInt(numbers_str[i]);
             sum = sum + count;
         }
-        System.out.println(sum);
+        System.out.println("Ответ: "+sum);
+    }
+    private static List<String> readLineByLine() throws IOException {
+        List<String> lines = new ArrayList<>();
+        Path path = Paths.get("//home//sasha//IdeaProjects//Start//src//com//company//Input");
+        try (Stream<String> lineStream = Files.lines(path)) {
+
+            lines = lineStream.collect(Collectors.toList());
+
+        } catch (IOException e) {
+          e.printStackTrace();
+        }
+        return lines;
     }
 }
 
